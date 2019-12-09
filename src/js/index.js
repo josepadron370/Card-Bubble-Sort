8d15c21bf0f83b0d962391abe4cf82287ced5bea
josepadron370 <josepadron370@gmail.com>
2019-12-09 22:16:20 +0000

    First Commit

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,21 @@ import "../style/index.scss";
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
+
+const bubbleSort = arr => {
+  let wall = arr.length - 1; //we start the wall at the end of the array
+  while (wall > 0) {
+    let index = 0;
+    while (index < wall) {
+      //compare the adjacent positions, if the right one is bigger, we have to swap
+      if (arr[index] > arr[index + 1]) {
+        let aux = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = aux;
+      }
+      index++;
+    }
+    wall--; //decrease the wall for optimization
+  }
+  return arr;
+};
